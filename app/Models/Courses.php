1200875ccc14ed90b82f94965633a33d45f11bf5
adrita06 <adritaahsan06@gmail.com>
2025-06-11 11:58:35 +0600

    implemented modules and resource management

--- app/Models/Courses.php
+++ app/Models/Courses.php
@@ -21,5 +21,9 @@ class Courses extends Model
 {
     return $this->hasMany(Enrollment::class);
 }
+ public function resources()
+    {
+    return $this->hasMany(Resource::class, 'courseId');
+    }
 
 }
