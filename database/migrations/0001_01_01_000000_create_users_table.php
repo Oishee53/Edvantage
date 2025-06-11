790986145a445b9dbe8eeb97dd6457d944cfe01a
Bohota1 <newazbohota@gmail.com>
2025-06-11 16:42:50 +0600

    without cache and job table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
         $table->id(); // auto-incremented primary key
         $table->string('name');
-        $table->string('email')->unique();
+        $table->string('email', 191)->unique();
         $table->string('phone')->nullable();
         $table->string('password');
         $table->string('field')->nullable();
