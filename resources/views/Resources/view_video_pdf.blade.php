e4615b09974e3f467b67cf2257d3ecb9c363cb89
adrita06 <adritaahsan06@gmail.com>
2025-06-20 23:38:26 +0600

    stored video in file system

--- resources/views/Resources/view_video_pdf.blade.php
+++ resources/views/Resources/view_video_pdf.blade.php
@@ -12,23 +12,11 @@
     @foreach ($resources as $resource)
     <tr>
         <p><strong>Module:</strong> {{ $resource->moduleId ?? 'N/A' }}</p>
-
-        @php
-            preg_match('/(?:v=|\/)([0-9A-Za-z_-]{11})/', $resource->videos, $matches);
-            $videoId = $matches[1] ?? null;
-        @endphp
-
         <div style="margin-bottom: 30px;">
             {{-- Display YouTube Video --}}
-            @if ($videoId)
-                <p><strong>YouTube Link:</strong></p>
-                <iframe width="560" height="315"
-                        src="https://www.youtube.com/embed/{{ $videoId }}"
-                        frameborder="0" allowfullscreen>
-                </iframe>
-            @else
-                <p>❌ Invalid YouTube URL</p>
-            @endif
+            <h3>Video Lecture</h3>
+            <a href="{{ route('video.view', ['filename' => basename($resource->videos)]) }}" target="_blank">🔗 Open video in New Tab</a>
+           
 
             {{-- Display PDF --}}
             <h3>PDF File:</h3>
