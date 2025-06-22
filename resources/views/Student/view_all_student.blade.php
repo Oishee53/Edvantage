9f8175279bd8be52718341a19d25ba4be4705790
adrita06 <adritaahsan06@gmail.com>
2025-06-22 09:22:54 +0600

    implemented view all student

--- resources/views/Student/view_all_student.blade.php
+++ resources/views/Student/view_all_student.blade.php
@@ -7,6 +7,18 @@
     <title>Document</title>
 </head>
 <body>
-    
+  <div class="container">
+    <h2 class="mb-4">All Students</h2>
+
+    @foreach($students as $student)
+        <div class="card mb-4">
+            <div class="card-header">
+                <strong>Name:</strong> {{ $student->name }} <br>
+                <strong>Email:</strong> {{ $student->email }} <br>
+                <strong>Phone:</strong> {{ $student->phone }}
+            </div>
+        </div>
+    @endforeach
+</div>  
 </body>
 </html>
