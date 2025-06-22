91ac95619cf2770ae6a86add6c139e4ac4b17957
adrita06 <adritaahsan06@gmail.com>
2025-06-22 18:23:57 +0600

    my profile frontend

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\AuthenticateUser;
 use App\Http\Middleware\PreventMultipleLogins;
+use App\Http\Middleware\PreventBackHistory;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -23,6 +24,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'auth.custom' => AuthenticateUser::class,
+        ]);
+        
+
+        $middleware->web(append: [
+        PreventBackHistory::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
